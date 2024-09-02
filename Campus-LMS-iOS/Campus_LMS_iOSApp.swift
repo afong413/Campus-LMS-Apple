7bd80930e478ab0e2705f480fdf176a7363d105a
Aiden Fong <afong413@gmail.com>
2024-09-02 12:25:51 -0700

    Added iOS target.

---
+++ Campus-LMS-iOS/Campus_LMS_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Campus_LMS_iOSApp.swift
+//  Campus-LMS-iOS
+//
+//  Created by Aiden Fong on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Campus_LMS_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

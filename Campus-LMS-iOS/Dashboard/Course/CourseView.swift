fa1d554a8ded3a326adcdc33c1cff38a8e5b4b1a
Aiden Fong <afong413@gmail.com>
2024-09-02 16:37:56 -0700

    Fetch assignments.

---
+++ Campus-LMS-iOS/Dashboard/Course/CourseView.swift
@@ -0,0 +1,18 @@
+//
+//  CourseView.swift
+//  Campus-LMS-iOS
+//
+//  Created by Aiden Fong on 9/2/24.
+//
+
+import SwiftUI
+
+struct CourseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CourseView()
+}

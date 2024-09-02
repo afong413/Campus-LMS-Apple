1c887c249934c97194aaff2092a6ae632132d2a0
Aiden Fong <afong413@gmail.com>
2024-09-02 15:55:52 -0700

    Restored.

---
+++ Campus-LMS-iOS/Dashboard/Course/Assignments/AssignmentView.swift
@@ -0,0 +1,18 @@
+//
+//  AssignmentView.swift
+//  Campus-LMS-iOS
+//
+//  Created by Aiden Fong on 9/2/24.
+//
+
+import SwiftUI
+
+struct AssignmentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AssignmentView()
+}
---
+++ Campus-LMS-iOS/Dashboard/Course/Assignments/AssignmentsHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  AssignmentsHomeView.swift
+//  Campus-LMS-iOS
+//
+//  Created by Aiden Fong on 9/2/24.
+//
+
+import SwiftUI
+
+struct AssignmentsHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AssignmentsHomeView()
+}
